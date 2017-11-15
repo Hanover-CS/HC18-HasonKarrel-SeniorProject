b65b5a19e14e5724e60d0d706617892d0928c5cd
Jason Karrel <jasonk20@gmail.com>
2017-11-15 14:09:00 -0500

    Added working toggle switch which saves current state (On/Off) to the device

--- MorseCodeMessenger/app/src/main/java/com/example/jasonk20/morsecodemessenger/Settings.java
+++ MorseCodeMessenger/app/src/main/java/com/example/jasonk20/morsecodemessenger/Settings.java
@@ -1,10 +1,15 @@
 package com.example.jasonk20.morsecodemessenger;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -12,6 +17,9 @@ public class Settings extends AppCompatActivity {
 
     private Button mLogOut;
     private FirebaseAuth mAuth;
+    private Button mPreset_Btn;
+    private Switch mCurrLocation_Swtich;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,8 @@ public class Settings extends AppCompatActivity {
 
         mLogOut = (Button) findViewById(R.id.logOut_Btn);
         mAuth = FirebaseAuth.getInstance();
+        mPreset_Btn = (Button) findViewById(R.id.preset_Btn);
+        mCurrLocation_Swtich = (Switch) findViewById(R.id.currLocation_Switch);
 
         mLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +38,32 @@ public class Settings extends AppCompatActivity {
                 Intent intent = new Intent(Settings.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        mPreset_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.this, PresetMessagesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mCurrLocation_Swtich.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor = sharedPref.edit();
+
+                if (isChecked) {
+                    editor.putString("SOS Functionality", "ON");
+                    editor.commit();
+                    Toast.makeText(Settings.this, "Toggle Switch ON",Toast.LENGTH_SHORT).show();
+                } else {
+                    editor.putString("SOS Functionality", "OFF");
+                    editor.commit();
+                    Toast.makeText(Settings.this, "Toggle Switch OFF",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
